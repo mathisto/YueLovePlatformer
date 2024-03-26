8da6a0b8165821c57d6ee45e244a5e8ddbf2b8a1
Matt Kelly <matthew.ryan.kelly@gmail.com>
2024-03-25 21:52:40 -0400

    Fix camera and simplfily tiled map

--- conf.lua
+++ conf.lua
@@ -2,11 +2,9 @@
 function love.conf(t)
     require("yue")
     t.console = true
-    t.window.title = "YueLOVE"
-    t.window.borderless = true
+    t.window.title = "YueLÖVE"
+    t.window.borderless = false
     t.window.resizable = true
     t.window.width = 1920
     t.window.height = 1080
-    t.modules.physics = false
-    t.modules.joystick = false
 end
